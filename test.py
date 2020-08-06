7b840553560f5d3003b232dfbba9a210ea92f72d
2892211452 <54618163+2892211452@users.noreply.github.com>
2020-08-06 12:55:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ CORS(app, supports_credentials=True)
 
 @app.route('/')
 def index():
-    return '<h1>hello</h1>'
+    return '<h1>hello 阿辉</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
